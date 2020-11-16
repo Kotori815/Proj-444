9de75a090e51d51f84357f9598fd77e1e4be8518
Kotori815 <990815_yxt@sina.com>
2020-11-16 23:00:27 +0800

    u2u ver.1

--- generation/test.py
+++ generation/test.py
@@ -1,8 +1,3 @@
 import bilibili_api as bili
-import re
-import json
-from util import Extractor, outputPath
-
 me = 31374926
-
 bili.user.get_followings_raw(31374926)
--- generation/user2user.py
+++ generation/user2user.py
@@ -18,16 +18,16 @@ current_layer, next_layer = start_list, list()
 get_cnt = 0
 try:
     for i in range(dig_depth):
-        print("expand layer {}".format(i+1))
-        print("total user: {}".format(len(current_layer)))
+        print('=====================================================')
+        print("expand layer {} with {} users in waiting list".format(i+1, len(current_layer)))
         s1, l = len(user_set), 0
 
         for user in tqdm.tqdm(current_layer):
             if user_set.get(user):
                 continue
 
-            followings = bili.user.get_followings_raw(user)
             get_cnt += 1
+            followings = bili.user.get_followings_raw(user)
             followings_info = json.dumps(followings)
             follow_list = extrac_uid.get_info_list(followings_info)
 
@@ -37,18 +37,20 @@ try:
 
             l += len(follow_list)
 
-        print("new user number: {}, new followings: {}".format(len(user_set) - s1, l))
-        
+        print("{} new users, {} new followings".format(len(user_set) - s1, l))
         current_layer, next_layer = next_layer, list()
 except:
+    # too much accessing (about 500) the server, access denied for 
+    # the following hour
     print("Getting error code 412")
     if random.random() < 0.1:
         print("Uncle Chen is watching you")
 
-print("total get number {}".format(get_cnt))
+print('=====================================================')
+print("times of accessing API {}".format(get_cnt))
 with open(outputPath + "users_all.json", 'w') as f:
     print("Wrinting {} users".format(len(user_set)))
-    json.dump(user_set, f)
+    json.dump(list(user_set.keys()), f)
 with open(outputPath + "followings.json", 'w') as f:
     print("Wrinting {} followings".format(l))
     json.dump(follow_network, f)
